dadeb52dbf39c4c748e956d3116dee66670327d0
Siddharth Batra <siddharthabatra@gmail.com>
2011-10-06 23:19:22 +0000

    uploading images directly to s3

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,7 +6,8 @@ class ProductsController < ApplicationController
   # Display UI for creating a new product
   #
   def new
-    @product = Product.new
+    @product  = Product.new
+    @uploader = generate_uploader
   rescue => ex
     handle_exception(ex)
   ensure
@@ -41,6 +42,38 @@ class ProductsController < ApplicationController
     handle_exception(ex)
   ensure
     redirect_to root_path if @error
+  end
+
+
+  protected
+
+  # Prepare parameters for the image uploder
+  #
+  def generate_uploader
+    uploader = {}
+    uploader[:server]     = "http://#{CONFIG[:s3_bucket]}.s3.amazonaws.com/"
+    uploader[:aws_id]     = CONFIG[:aws_access_id]
+    uploader[:max]        = 50.megabytes
+    uploader[:extensions] = "*.jpeg;*.jpg;*.gif;*.bmp;*.png"
+    uploader[:key]        = [Time.now.to_i,SecureRandom.hex(20)].join("_")
+    uploader[:policy]     = Base64.encode64(
+                              "{'expiration': '#{6.year.from_now.
+                                  utc.strftime('%Y-%m-%dT%H:%M:%S.000Z')}',
+                                'conditions': [
+                                  {'bucket': '#{CONFIG[:s3_bucket]}'},
+                                  {'acl': 'public-read'},
+                                  {'success_action_status': '201'},
+                                  ['content-length-range',0, #{uploader[:max]}],
+                                  ['starts-with', '$key', '#{uploader[:key]}'],
+                                  ['starts-with', '$Filename', '']
+                                ]
+                              }").gsub(/\n|\r/, '')
+    uploader[:signature]   = Base64.encode64(
+                              OpenSSL::HMAC.digest(
+                                OpenSSL::Digest::Digest.new('sha1'),
+                                CONFIG[:aws_secret_key],
+                                uploader[:policy])).gsub("\n","")
+    uploader
   end
 
 end
