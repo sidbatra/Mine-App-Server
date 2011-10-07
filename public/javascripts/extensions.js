a037538f893621570c56ead5737e8378b541330a
Siddharth Batra <siddharthabatra@gmail.com>
2011-10-07 21:18:53 +0000

    adding extensionms

---
+++ public/javascripts/extensions.js
@@ -0,0 +1,7 @@
+
+// Add proper case functionality to strings
+//
+String.prototype.toProperCase = function () {
+    return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+};
+
