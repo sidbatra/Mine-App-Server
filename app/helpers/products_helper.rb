00dab2abd22582a0bb64ec457e6fbd1cc4947161
Siddharth Batra <siddharthabatra@gmail.com>
2011-10-07 21:11:40 +0000

    conditional clear fields added for unobtrusive javascript (partially true)

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,4 +1,14 @@
 module ProductsHelper
 
+# Generate a clear field that clears the text
+# only if the field is left empty
+#
+def conditional_clear_field(form,name,text)
+  form.text_field name,
+                :value    => text,
+                :onfocus  => "if(this.value=='#{text}')this.value='';this.style.color='#333333';",
+                :onblur   => "if(this.value==''){this.value='#{text}';this.style.color='#999999';}"
+end
+
 
 end
