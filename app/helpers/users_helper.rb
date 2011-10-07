63473f526150308ba89b47d822bc32cf2ad9b1a5
Siddharth Batra <siddharthabatra@gmail.com>
2011-10-07 01:36:31 +0000

    style layer added for product show page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+  
+  # Generate text for a user to share
+  #
+  def user_share_text(user)
+    [user.first_name,
+      user.last_name, 
+      "shared",
+      user.gender == "male" ? "his" : "her"].join(' ')
+  end
 end
