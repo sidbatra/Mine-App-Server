0611b8e368d9d68e835757de6eebb3823f14aa34
Siddharth Batra <siddharthabatra@gmail.com>
2011-10-11 01:20:55 +0000

    displaying the first 6 products in random order on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,11 @@
+# Handle requests for the home page
+#
 class HomeController < ApplicationController
 
+  # Display pre-selected products on the home page
+  #
   def show
-    @products = Product.find_all_by_id([1,10,110,150])
+    @products = Product.find_all_by_id([1,2,3,4,5,6])
   end
 
 end
