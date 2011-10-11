2ac1f05d4d4cc8ba3783f9c815284e41a8b2f5b4
Siddharth Batra <siddharthabatra@gmail.com>
2011-10-11 23:16:24 +0000

    remove randomness from home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
   # Display pre-selected products on the home page
   #
   def show
-    @products = Product.find_all_by_id([1,2,3,4,5,6]).shuffle
+    @products = Product.find_all_by_id([1,2,3,4,5,6])
   end
 
 end
