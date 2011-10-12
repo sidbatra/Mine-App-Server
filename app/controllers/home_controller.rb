5a59214bd2e86d1038c49a8c3274dee117761f58
Siddharth Batra <siddharthabatra@gmail.com>
2011-10-12 20:06:57 +0000

    dynamic variable campaign added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,7 @@ class HomeController < ApplicationController
   # Display pre-selected products on the home page
   #
   def show
+    @campaign = params[:id]
     @products = Product.eager.find_all_by_id([1,2,3,4,5,6])
   end
 
