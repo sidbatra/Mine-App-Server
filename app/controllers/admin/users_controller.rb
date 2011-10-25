bd889b6c0ead95224abb08f0704d56c13c48c878
Deepak Rao <raodpk@gmail.com>
2011-10-25 19:20:15 +0000

    cleaned up code for calculation age-buckets in admin/user view

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -6,17 +6,15 @@ class Admin::UsersController < ApplicationController
   # Show all the users who have created a product
   # 
   def index 
-    age_bucket        = 5
-
     @users            = Product.eager.all.map(&:user).uniq
     @grouped_users    = {} 
 
     @users.each do |user| 
-      key = user.age/age_bucket
+      key = user.age/CONFIG[:age_bucket_size]
       @grouped_users[key] = [] unless @grouped_users.has_key?(key)
       @grouped_users[key] = @grouped_users[key] << user 
     end
 
-      @grouped_users = @grouped_users.sort{|a,b| b[1].length<=>a[1].length}
+    @grouped_users = @grouped_users.sort{|a,b| a[0]<=>b[0]}
   end
 end
--- app/helpers/admin/users_helper.rb
+++ app/helpers/admin/users_helper.rb
@@ -1,2 +1,10 @@
 module Admin::UsersHelper
+
+  # Generate age range from the bucket number
+  #
+  def age_range(bucket)
+    (bucket*CONFIG[:age_bucket_size]).to_s + "--" + 
+    ((bucket+1)*CONFIG[:age_bucket_size]-1).to_s
+  end
+
 end
