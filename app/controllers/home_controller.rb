5fb6b12254778915b6e02915ab1e122aa77a8586
Siddharth Batra <siddharthabatra@gmail.com>
2011-10-30 00:53:57 +0000

    simple a/b testing for home layouts and show pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,28 @@
 # Handle requests for the home page
 #
 class HomeController < ApplicationController
-layout 'home'
+layout :decide_layout
 
   # Display pre-selected products on the home page
   #
   def show
-    @source    = params[:id] ? params[:id].to_s : "unknown"
+    @source   = params[:id] ? params[:id].to_s : "unknown"
+    @layout   = 'home1'
+    @layout   = 'home2' if @source == "home2"
 
     redirect_to user_path(self.current_user) if logged_in?
+  
+    page = "show1"
+    page = "show2" if @layout == "home2"
+    render page
+  end
+
+  private 
+
+  # Decide layout based on the source of the visit to home page
+  #
+  def decide_layout
+    @layout
   end
 
 end
