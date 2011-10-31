34a1858d0ecb224ae237bcb8ed7749b1a7a27b1f
Siddharth Batra <siddharthabatra@gmail.com>
2011-10-31 21:42:13 +0000

    adding comments on comment model

--- public/javascripts/models/comment.js
+++ public/javascripts/models/comment.js
@@ -1,10 +1,19 @@
+// The Comment model represents user comments
+// on products
+//
 Denwen.Comment = Backbone.Model.extend({
 
+  // Route on the app server
+  //
   urlRoot: '/comments',
 
+  // Constructor logic
+  //
   initialize: function() {
   },
 
+  // Validation logic
+  //
   validate: function(attrs) {
     if(attrs.data.length < 1 || attrs.data == this.get('defaultData')) 
       return "No comment entered";
