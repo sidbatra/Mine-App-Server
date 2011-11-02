6e82883dc413e5180a5983631354a9f8394e0bdf
Pierre Legrain <pierrelegrain@gmail.com>
2011-11-02 21:12:23 +0000

    product view rejigging (unifinished)

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,7 +5,7 @@ module UsersHelper
   def user_share_text(user)
     [user.first_name,
       user.last_name, 
-      "shared",
+      "bought",
       user.gender == "male" ? "his" : "her"].join(' ')
   end
   
@@ -32,13 +32,13 @@ module UsersHelper
   def category_create_link_text(category)
     category.id == 8 ? 
             "Add anything!" :
-            "Add your #{category.name.downcase}" 
+            "Add your #{category.name.downcase} ›" 
   end
 
 
   # Display text for category when used in a filter link
   def category_filter_link_text(category,user)
-    "#{category.name} " + "<span class='cat_num'>(#{user.products_category_count(category.id)})</span><br>" 
+    "#{category.name} " + "<span class='cat_num'>#{user.products_category_count(category.id)}</span><br>" 
   end
 
 
