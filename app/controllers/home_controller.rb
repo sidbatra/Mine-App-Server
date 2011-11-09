6495c307f0fec1669985e15f0e1e6e61e9496f9b
Siddharth Batra <siddharthabatra@gmail.com>
2011-11-09 00:11:15 +0000

    new home page setup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@
 #
 class HomeController < ApplicationController
   before_filter :detect_origin
-  layout        :decide_layout
+  layout        'home'
 
   # Display pre-selected products on the home page
   #
@@ -10,10 +10,9 @@ class HomeController < ApplicationController
     if params[:id]
       redirect_to root_path
     elsif logged_in? 
-      redirect_to user_path(self.current_user,:src => "home_redirect") 
+      redirect_to user_path(self.current_user,:src => 'home_redirect') 
     else
-      @source = params[:src] ? params[:src].to_s : "direct"
-      @layout = @origin == "sbaf2" ? "home2" : "home1"
+      @source = params[:src] ? params[:src].to_s : 'direct'
     end
   end
 
@@ -22,14 +21,15 @@ class HomeController < ApplicationController
   # Decide layout based on the source of the visit to home page
   #
   def decide_layout
-    @layout
   end
 
   # Detect which origin the user is coming from and save it to session
   #
   def detect_origin
-    session[:origin]  ||= params[:id] ? params[:id].to_s : "direct"
+    session[:origin]  ||= params[:id] ? params[:id].to_s : 'direct'
+    session[:home]    ||= ['girl','tri'][rand(2)]
     @origin             = session[:origin]
+    @home               = session[:home]
   end
 
 end
