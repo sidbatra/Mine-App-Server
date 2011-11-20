21c27c532b547389db6c5d2fe24ee15c51357094
Siddharth Batra <siddharthabatra@gmail.com>
2011-11-20 01:54:35 +0000

    switching to amazon elasticache for memcached

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,5 +18,5 @@ ActionController::Base.asset_host = File.join(
 
 
 # Cache settings
-config.cache_store = :mem_cache_store, '107.20.229.8:11211'
+config.cache_store = :mem_cache_store, 'denwen-felvy.afhe3c.0001.use1.cache.amazonaws.com:11211'
 config.action_controller.page_cache_directory = RAILS_ROOT + "/public/cache"
