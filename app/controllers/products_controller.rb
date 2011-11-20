6d4940fb7a9d5150201882605703cd2d70348d37
Siddharth Batra <siddharthabatra@gmail.com>
2011-11-20 04:17:13 +0000

    changing unknown store_id to 0

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -175,7 +175,7 @@ class ProductsController < ApplicationController
                                     Store.add(
                                         product_params[:store_name],
                                         self.current_user.id).id :
-                                    nil
+                                    0
     end
 
     if product_params[:is_gift] && 
