5dea456533bfd1d237e2f5155fdfb252ed2a388e
Deepak Rao <raodpk@gmail.com>
2011-12-01 23:36:27 +0000

    invite changed to batch invite to decrease the server requests

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -6,7 +6,11 @@ class InvitesController < ApplicationController
   # Create an invite 
   #
   def create
-    @invite = Invite.add(self.current_user.id,params[:fb_user_id])
+    @invite = nil
+
+    params[:fb_user_ids].each do |fb_user_id|
+      Invite.add(self.current_user.id,fb_user_id)
+    end
   rescue => ex
     handle_exception(ex)
   ensure
