338b2c367d5d0d647e59480efe2dec6af562b7ae
Siddharth Batra <siddharthabatra@gmail.com>
2011-12-01 00:24:25 +0000

    deploy daemon

---
+++ lib/daemons/deploy.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+# Automatically deploy releases when a change is detected
+#
+
+$running = true
+Signal.trap("TERM") do 
+  $running = false
+end
+
+while($running) do
+
+  begin 
+    if `cd #{ENV['RAILS_PATH']} && git pull`.chomp != "Already up-to-date."
+      `cd #{ENV['RAILS_PATH']} && cap #{ENV['RAILS_ENV']} deploy:release`
+      puts "Released at - #{Time.now}"
+    end
+  rescue => ex
+    LoggedException.add(__FILE__,__method__,ex)
+  end
+
+  sleep 10
+end
