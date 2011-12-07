43d4f3f07b34ee76db2138e1427b5a20691f9c49
Deepak Rao <raodpk@gmail.com>
2011-12-07 01:49:53 +0000

    user's store implemented

--- public/javascripts/views/users/show.js
+++ public/javascripts/views/users/show.js
@@ -31,6 +31,11 @@ Denwen.Views.Users.Show = Backbone.View.extend({
                                   this.user.get('inverse_followings_count') <= 0
                                   && this.source == 'login'});
     // -----
+    new Denwen.Partials.Users.Stores({
+                          el    : '#user_stores_box',
+                          user  : this.user});
+
+    // -----
     new Denwen.Partials.Users.Stars({
                           el  : '#star_users_box'});
 
