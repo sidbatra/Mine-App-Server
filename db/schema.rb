0264132e03c160f269759e6cf60cb2af02dc3384
Siddharth Batra <siddharthabatra@gmail.com>
2011-12-07 02:06:51 +0000

    adding index on product created_at

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111207013413) do
+ActiveRecord::Schema.define(:version => 20111207020210) do
 
   create_table "actions", :force => true do |t|
     t.integer  "product_id"
