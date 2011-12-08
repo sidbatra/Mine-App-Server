5ef7306c305b0bc731839a5d9eae6ebddbe10c7f
Siddharth Batra <siddharthabatra@gmail.com>
2011-12-08 05:02:45 +0000

    Bug fix: nil source values for old Following rows causing validation errors. The source value for all Following rows was nil and led to validation when an existing following was toggled between active and inactive.
    Also fixing a bug in following observer which updates counter_caches
    whenever following is updated irresepective of is_active being toggled.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111208003516) do
+ActiveRecord::Schema.define(:version => 20111208043431) do
 
   create_table "actions", :force => true do |t|
     t.integer  "user_id"
