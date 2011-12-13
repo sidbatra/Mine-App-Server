db416384a3f1024713a77a66682b23e0307667a0
Deepak Rao <raodpk@gmail.com>
2011-12-13 22:38:25 +0000

    basic framework for achivement and achivements_set model

---
+++ app/models/achievement.rb
@@ -0,0 +1,32 @@
+class Achievement < ActiveRecord::Base
+
+  #-----------------------------------------------------------------------------
+  # Associations
+  #-----------------------------------------------------------------------------
+  belongs_to  :achievement_set
+  belongs_to  :achievable, :polymorphic => true
+
+  #-----------------------------------------------------------------------------
+  # Validations
+  #-----------------------------------------------------------------------------
+  validates_presence_of   :achievable_id
+  validates_presence_of   :achievable_type
+  validates_inclusion_of  :achievable_type, :in => %w(User Product)
+  validates_presence_of   :user_id
+  validates_presence_of   :achievement_set_id
+
+  #-----------------------------------------------------------------------------
+  # Class methods
+  #-----------------------------------------------------------------------------
+
+  # Add a new achievement 
+  #
+  def self.add(entity,achievement_set_id,user_id)
+    create!(
+      :achievable_id        => entity.id,
+      :achievable_type      => entity.class.name,
+      :achievement_set_id   => achievement_set_id,
+      :user_id              => user_id)
+  end
+
+end
---
+++ app/models/achievement_set.rb
@@ -0,0 +1,38 @@
+class AchievementSet < ActiveRecord::Base
+
+  #-----------------------------------------------------------------------------
+  # Associations
+  #-----------------------------------------------------------------------------
+  has_many :achievements, :dependent => destroy
+
+  #-----------------------------------------------------------------------------
+  # Validations
+  #-----------------------------------------------------------------------------
+  validates_presence_of   :owner_id
+  validates_presence_of   :type
+  validates_inclusion_of  :type, :in => %w(star_users top_shoppers)
+
+  #-----------------------------------------------------------------------------
+  # Class methods
+  #-----------------------------------------------------------------------------
+
+  # Add a new achievement set 
+  #
+  def self.add(owner_id,type)
+    create!(
+      :owner_id   => owner_id,
+      :type       => type)
+  end
+
+  #-----------------------------------------------------------------------------
+  # Instance methods
+  #-----------------------------------------------------------------------------
+
+  # Add an expiry date to the achivement set
+  #
+  def expire
+    self.expired_at = Time.now
+    self.save!
+  end
+
+end
