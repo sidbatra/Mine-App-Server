7fcaf0ac103aa400aebdf254e537a7d92b7dd2d4
Deepak Rao <raodpk@gmail.com>
2011-12-14 00:02:28 +0000

    moving the star_users method again

--- app/models/achievement_set.rb
+++ app/models/achievement_set.rb
@@ -24,6 +24,15 @@ class AchievementSet < ActiveRecord::Base
       :type       => type)
   end
 
+  # Fetch the star users from the database. This set of users is the
+  # one which will be displayed on the website at any given time
+  #
+  def self.star_users
+    achievement_set = find_last_by_type('star_users')
+    Achievement.achievers(achievement_set.id)
+  end
+
+
   #-----------------------------------------------------------------------------
   # Instance methods
   #-----------------------------------------------------------------------------
