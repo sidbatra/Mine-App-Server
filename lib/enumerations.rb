a93ea7bc60dc27ecbc4c1fe1e29bce2e6c5d29c8
Siddharth Batra <siddharthabatra@gmail.com>
2011-12-22 23:50:24 +0000

    Enumeration base class added for structuring enumerations.

--- lib/enumerations.rb
+++ lib/enumerations.rb
@@ -4,9 +4,23 @@ module DW
   #
   module Enumerations
 
+    # Base class for all enumerations
+    #
+    class Enumeration
+      
+      # Return a range starting at 0 and till the
+      # total length of constants in the class. This is useful
+      # if the Enumeration holds types that map to ActiveRecord
+      # columns that require validation for inclusion
+      #
+      def self.range
+        (0..constants.length-1)
+      end
+    end
+
     # Types of sources for the Shopping model
     #
-    class ShoppingSource
+    class ShoppingSource < Enumeration
       User    = 0
       Product = 1
     end
