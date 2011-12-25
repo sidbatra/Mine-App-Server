888f5dea4d941b40f32c64ac15f294037be744f6
Siddharth Batra <siddharthabatra@gmail.com>
2011-12-25 00:48:10 +0000

    adding stores association for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,7 @@ class User < ActiveRecord::Base
   has_many :collections,  :dependent => :destroy
   has_many :achievements, :dependent => :destroy
   has_many :shoppings,    :dependent => :destroy
+  has_many :stores,       :through   => :shoppings
 
   has_many :followings, :dependent  => :destroy
   has_many :followers,  :through    => :followings
