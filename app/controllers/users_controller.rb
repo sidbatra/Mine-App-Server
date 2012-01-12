9f68053645775705c1bb7cf1377c77538acfafb9
Deepak Rao <raodpk@gmail.com>
2012-01-12 20:59:33 +0000

    Intro page reintroduced in onboarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,7 +36,7 @@ class UsersController < ApplicationController
     self.current_user = @user
     set_cookie
     target_url = @user.is_fresh ? 
-                  welcome_path(WelcomeFilter::Stores) :
+                  welcome_path(WelcomeFilter::Learn) :
                   user_path(@user.handle,:src => 'login')
 
   rescue => ex
