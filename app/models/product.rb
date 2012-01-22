1128cad9090a1c0d1ef561c56af0b8d3bc3be225
Siddharth Batra <siddharthabatra@gmail.com>
2012-01-22 20:10:49 +0000

    Removing self likes from user profiles

--- app/models/product.rb
+++ app/models/product.rb
@@ -40,6 +40,8 @@ class Product < ActiveRecord::Base
                               {:conditions => {:user_id => user_id}}}
   named_scope :for_store,   lambda {|store_id| 
                               {:conditions => {:store_id => store_id}}}
+  named_scope :not_for_user, lambda {|user_id| 
+                              {:conditions => {:user_id_ne => user_id}}}
   named_scope :acted_on_by_for, lambda {|user_id,name| 
                                   {:conditions => {:actions => {
                                                     :user_id  => user_id,
