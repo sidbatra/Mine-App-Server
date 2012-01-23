745921656b6931dcc34e233e61eea9db1f9f7e39
Deepak Rao <raodpk@gmail.com>
2012-01-23 22:15:19 +0000

    reverting back to old facebook connect

--- public/javascripts/views/welcome/show.js
+++ public/javascripts/views/welcome/show.js
@@ -1,19 +1,24 @@
-// View for displaying the site introduction during onboarding
+// View for welcoming the user during onboarding
 //
 Denwen.Views.Welcome.Show = Backbone.View.extend({
   
   // Constructor logic
   //
   initialize: function() {
+    this.source = this.options.source;
+
+    // -----
     this.setAnalytics();
   },
 
   // Fire tracking events
   //
   setAnalytics: function() {
-    analytics.fbConnectAccepted();
     analytics.welcomeView();
-    analytics.userCreated(); 
+
+    if(this.source == 'login') {
+      analytics.userCreated(); 
+    }
   }
 
 });
