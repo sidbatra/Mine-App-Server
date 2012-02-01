9c9dd90fa196ad9e055a68fb5d34de77f2f67962
Deepak Rao <raodpk@gmail.com>
2012-02-01 00:03:08 +0000

    arrows functional in collection/show

--- app/presenters/collection_presenter.rb
+++ app/presenters/collection_presenter.rb
@@ -3,6 +3,18 @@
 class CollectionPresenter < BasePresenter
   presents :collection
 
+  # Relative path for the collection 
+  #
+  def path(source)
+    h.collection_path collection.user.handle,collection.id,:src => source
+  end
+  
+  # Absolute path for the collection 
+  # 
+  def url(source)
+    h.collection_url collection.user.handle,collection.id,:src => source
+  end
+
   # Link to the next collection 
   #
   def next(next_collection,user)
@@ -21,6 +33,14 @@ class CollectionPresenter < BasePresenter
             '',
             collection_path(user.handle,prev_collection.id,:src => 'previous'),
             :class => 'previous') : ''
+  end
+
+  # Link to go get back to the source url
+  #
+  def breadcrumb(title,path)
+    h.link_to "← " + title,
+              path,
+              :class => '' 
   end
 
 end
