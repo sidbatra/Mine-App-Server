d03292a7c9e8ddabb2477d453396d68fdb2d5e57
Deepak Rao <raodpk@gmail.com>
2012-02-14 21:57:00 +0000

    migration for converting likes to wants

---
+++ db/migrate/20120214211658_convert_likes_to_wants.rb
@@ -0,0 +1,16 @@
+class ConvertLikesToWants < ActiveRecord::Migration
+  def self.up
+    Action.find_all_by_name('like', :include => :actionable).each do |action|
+      Action.find_or_create_by_actionable_id_and_actionable_type_and_name_and_user_id(
+              action.actionable_id,
+              action.actionable_type,
+              ActionName::Want,
+              action.user_id) unless action.user_id == action.actionable.user_id
+
+      action.destroy
+    end
+  end
+
+  def self.down
+  end
+end
