93d53a057fb3a8abae7f5d8a79eb2c332f8ce7cb
Deepak Rao <raodpk@gmail.com>
2012-02-14 23:02:05 +0000

    converting likes to wants only for products

--- db/migrate/20120214211658_convert_likes_to_wants.rb
+++ db/migrate/20120214211658_convert_likes_to_wants.rb
@@ -1,6 +1,10 @@
 class ConvertLikesToWants < ActiveRecord::Migration
   def self.up
-    Action.find_all_by_name('like', :include => :actionable).each do |action|
+    Action.find_all_by_name(
+            ActionName::Like, 
+            :include    => :actionable,
+            :conditions => {:actionable_type => 'Product'}).each do |action|
+
       Action.find_or_create_by_actionable_id_and_actionable_type_and_name_and_user_id(
               action.actionable_id,
               action.actionable_type,
