1efe13b53103f7ee0b1aa05d5ad1b144996ef0a8
Deepak Rao <raodpk@gmail.com>
2012-02-15 21:04:24 +0000

    faster migration for converting wants to likes this migration wont send emails on action creation as actions are populated by mass inserts bypassing all rails observers Another thing to do before the release is to clear or cache that would have been usually deleted after action creation

--- db/migrate/20120214211658_convert_likes_to_wants.rb
+++ db/migrate/20120214211658_convert_likes_to_wants.rb
@@ -1,18 +1,31 @@
 class ConvertLikesToWants < ActiveRecord::Migration
   def self.up
+
+    columns = [:actionable_id, :actionable_type, :name, :user_id]
+    values  = []
+    ids     = []
+
     Action.find_all_by_name(
             ActionName::Like, 
             :include    => :actionable,
             :conditions => {:actionable_type => 'Product'}).each do |action|
 
-      Action.find_or_create_by_actionable_id_and_actionable_type_and_name_and_user_id(
-              action.actionable_id,
-              action.actionable_type,
-              ActionName::Want,
-              action.user_id) unless action.user_id == action.actionable.user_id
+      values << [action.actionable_id,
+                action.actionable_type,
+                ActionName::Want,
+                action.user_id] unless action.user_id == action.actionable.user_id
 
-      action.destroy
+      ids << action.id
     end
+
+    Action.destroy_all(:id => ids)
+
+    Action.import columns, values
+
+    columns = [:actions_count]
+    values  = Product.all(:include => :actions).map{|p| [p.actions.length]} 
+
+    Product.import columns, values, {:validate => false}
   end
 
   def self.down
