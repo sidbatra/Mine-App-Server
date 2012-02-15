a4b2ce2e11cb55ca56a80b0003381fb6bb301847
Siddharth Batra <siddharthabatra@gmail.com>
2012-02-15 20:30:46 +0000

    Instance destroy method added

--- lib/instance_management.rb
+++ lib/instance_management.rb
@@ -19,16 +19,16 @@ module DW
                 :stopping     => "64",
                 :stopped      => "80"}
 
+      @ec2 = AWS::EC2::Base.new(
+              :access_key_id      => CONFIG[:aws_access_id],
+              :secret_access_key  => CONFIG[:aws_secret_key])
+
 
       # Fetch all instances that match the provided
       # filtering options
       #
       def self.all(options={})
-        ec2 = AWS::EC2::Base.new(
-                :access_key_id      => CONFIG[:aws_access_id],
-                :secret_access_key  => CONFIG[:aws_secret_key])
-
-        instances = ec2.describe_instances.reservationSet.item.map do |group| 
+        instances = @ec2.describe_instances.reservationSet.item.map do |group| 
                       group.instancesSet.item
                     end.flatten
 
@@ -65,18 +65,28 @@ module DW
       # Create fresh instances
       #
       def self.create(options={})
-        ec2 = AWS::EC2::Base.new(
-                :access_key_id      => CONFIG[:aws_access_id],
-                :secret_access_key  => CONFIG[:aws_secret_key])
-
-        response = ec2.run_instances(options[:instances])
+        response = @ec2.run_instances(options[:instances])
+        instances = response.instancesSet.item
         sleep 3
 
         if tags_are_valid? options[:tags]
-          ec2.create_tags(
-            :resource_id  => response.instancesSet.item.map(&:instanceId),
+          @ec2.create_tags(
+            :resource_id  => instances.map(&:instanceId),
             :tag          => options[:tags].map{|k,v| {k.to_s.capitalize => v}})
         end
+
+        instances
+      end
+
+      # Destroy instances based on the tags provided
+      #
+      def self.destroy(options={})
+        instances = all(options)
+        instances = instances[0..options[:count]-1] if options[:count]
+
+        @ec2.terminate_instances(:instance_id => instances.map(&:instanceId))
+
+        instances
       end
 
 
