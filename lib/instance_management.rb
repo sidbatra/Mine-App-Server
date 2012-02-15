567efd51ba5d190f767bd8b78e714f842ce11cc6
Siddharth Batra <siddharthabatra@gmail.com>
2012-02-15 19:28:31 +0000

    asbtracting instace_management into a lib

---
+++ lib/instance_management.rb
@@ -0,0 +1,70 @@
+module DW
+
+  # Set of classes and methods for instace (server) management
+  #
+  module InstanceManagement
+
+    require "AWS"
+    
+    # Manage tasks instance related tasks such as
+    # fetching, creation, and deletion.
+    #
+    class Instance
+
+      @states = {
+                :pending      => "0",
+                :running      => "16", 
+                :shuttingdown => "32",
+                :terminated   => "48",
+                :stopping     => "64",
+                :stopped      => "80"}
+
+      # Fetch all instances that match the provided
+      # filtering options
+      #
+      def self.all(options={})
+        ec2 = AWS::EC2::Base.new(
+                :access_key_id      => CONFIG[:aws_access_id],
+                :secret_access_key  => CONFIG[:aws_secret_key])
+
+        instances = ec2.describe_instances.reservationSet.item.map do |group| 
+                      group.instancesSet.item
+                    end.flatten
+
+        if options[:state]
+          instances = instances.select do |instance|
+                        next unless instance.instanceState.code == 
+                                      @states[options[:state]]
+                        true
+                      end
+        end
+
+        if options[:tags] && 
+            options[:tags].is_a?(Hash) && 
+            options[:tags].length > 0
+
+          instances = instances.select do |instance|
+                        tags = instance.tagSet.item
+                        next unless tags.length > 0 
+                        
+                        failed = false
+
+                        tags.each do |tag|
+                          key = tag.key.downcase.to_sym
+                          failed = true if options[:tags][key] &&
+                                            options[:tags][key] != tag.value
+                        end
+
+                        next if failed
+                        true
+                      end
+        end
+
+        instances
+      end
+
+    end #instance manager
+
+  end #instance management
+
+end #dw
