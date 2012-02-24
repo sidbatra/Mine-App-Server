4c682f00d37d09809888876c9985ae89d217d135
Siddharth Batra <siddharthabatra@gmail.com>
2012-02-24 18:44:35 +0000

    use handler to generate handles for each collection

--- db/migrate/20120224183425_add_handle_to_collections.rb
+++ db/migrate/20120224183425_add_handle_to_collections.rb
@@ -1,6 +1,8 @@
 class AddHandleToCollections < ActiveRecord::Migration
   def self.up
     add_column :collections, :handle, :string
+
+    Collection.all.each{|c| c.save!}
   end
 
   def self.down
