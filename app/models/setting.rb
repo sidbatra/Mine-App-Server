6d425dfa19a47114435813685dafdd9c1a7d060d
Siddharth Batra <siddharthabatra@gmail.com>
2012-02-25 22:00:29 +0000

    email settings added to settings page

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,6 +1,7 @@
 class Setting < ActiveRecord::Base
   belongs_to :user
-  attr_accessible :post_to_timeline
+  attr_accessible :post_to_timeline, :email_interaction, 
+                    :email_influencer, :email_update
 
   #----------------------------------------------------------------------
   # Validations
