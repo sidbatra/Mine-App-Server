17462ddd15571586e4991ee1bfc1b4f2c5e1b8a7
Siddharth Batra <siddharthabatra@gmail.com>
2012-02-25 21:09:44 +0000

    cron job to ping users to create another collection

--- config/whenever/cron.rb
+++ config/whenever/cron.rb
@@ -5,6 +5,10 @@ every 1.hour do
   rake "upload_logs_to_dump"
 end
 
+every 1.day, :at => '3:00pm' do
+  runner "ProcessingQueue.push(CronWorker,:email_to_create_another_collection)"
+end
+
 every 1.week, :at => '2:00am' do
   runner "ProcessingQueue.push(CronWorker,:update_top_shoppers)"
 end
