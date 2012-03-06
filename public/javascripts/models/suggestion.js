cab0ed2974146b7e8619ee182d949d3f0196b28b
Siddharth Batra <siddharthabatra@gmail.com>
2012-03-06 02:58:12 +0000

    model & collection classes for the suggestion entity

---
+++ public/javascripts/models/suggestion.js
@@ -0,0 +1,14 @@
+//
+//
+Denwen.Models.Suggestion = Backbone.Model.extend({
+
+  // Route on the app server
+  //
+  urlRoot: '/suggestions',
+
+  // Constructor logic
+  //
+  initialize: function() {
+  }
+
+});
