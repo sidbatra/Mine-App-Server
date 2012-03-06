6027e0c033ad4a78c75be0aca9693db12a809a4c
Siddharth Batra <siddharthabatra@gmail.com>
2012-03-06 19:17:06 +0000

    apply active class on onboarding steps

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,13 @@
 module WelcomeHelper
+
+  # Apply an active class on the currently highlighted
+  # step during onboarding.
+  #
+  # filter String representing step to be tested
+  #
+  # String active class or empty string
+  #
+  def apply_active_class(filter)
+    @filter == filter ? 'active' : ''
+  end
 end
