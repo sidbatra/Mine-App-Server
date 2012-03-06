1100557c15d4e5c01166a71360b131c2f5106fe8
Siddharth Batra <siddharthabatra@gmail.com>
2012-03-06 01:16:50 +0000

    abstracting suggestion html + js into partials for reuse in edit

--- public/javascripts/views/admin/suggestions/new.js
+++ public/javascripts/views/admin/suggestions/new.js
@@ -10,36 +10,8 @@ Denwen.Views.Admin.Suggestions.New = Backbone.View.extend({
   // Constructor logic
   //
   initialize: function() {
-    this.imagePathEl        = $('#suggestion_image_path');
-    this.imageContainerEl   = $('#suggestion_image');
-
-    this.uploadConfig = this.options.uploadConfig;
-
-    this.imageUploader = new Denwen.Partials.Common.Uploader({
-                              el : $('#image_uploader'),
-                          config : this.uploadConfig});
-
-    this.imageUploader.bind(
-      Denwen.Callbacks.Uploader.FileUploadDone,
-      this.imageUploadDone,
-      this);
-
-    this.imageUploader.bind(
-      Denwen.Callbacks.Uploader.FileUploadError,
-      this.fileUploadError,
-      this);
-  },
-
-  // Callback from imageUploader - file is uploaded
-  //
-  imageUploadDone: function(file,relativePath,absolutePath) {
-    this.imagePathEl.val(relativePath);
-    this.imageContainerEl.html("<img src='" + absolutePath + "' />");
-  },
-
-  // Callback from uploader - error uploading file
-  //
-  fileUploadError: function(message) {
-    dDrawer.error('Error uploading file: ' + message);
+    new Denwen.Partials.Admin.Suggestions.Input({
+                    el:this.el,
+          uploadConfig:this.options.uploadConfig});
   }
 });
