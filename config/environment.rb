e1eba3176a6cc6e2c0af67c5dfd3d7dd8efcc6dc
Siddharth Batra <siddharthabatra@gmail.com>
2012-03-14 01:22:24 +0000

    update SuggestionCacheKey configuration after changes to suggestions

--- config/environment.rb
+++ config/environment.rb
@@ -87,7 +87,7 @@ Rails::Initializer.run do |config|
                                     :user_sweeper, :achievement_set_sweeper,
                                     :comment_sweeper, :action_sweeper,
                                     :collection_sweeper, :specialty_sweeper,
-                                    :shopping_sweeper
+                                    :shopping_sweeper, :suggestion_sweeper
 
   # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
   # Run "rake -D time" for a list of tasks for finding time zone names.
