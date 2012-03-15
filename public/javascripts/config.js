0be5d5c9843c077006467895119db8a7e0bc9987
Siddharth Batra <siddharthabatra@gmail.com>
2012-03-15 23:56:18 +0000

    hide View All when entries less than 7

--- public/javascripts/config.js
+++ public/javascripts/config.js
@@ -4,10 +4,11 @@
 // and rerun "rake js:dump:config"
 // 
 CONFIG = {
+'multi_query_msg' : "<strong>Tip:</strong> Add items by URL for the perfect photo every time.",
 'multi_query_threshold' : 5,
 'minimum_products_in_collection' : 2,
 'fb_extended_permissions' : "publish_stream",
-'one_word_query_msg' : "<strong>Tip:</strong> Including brands and colors helps you find your item.",
 'products_threshold' : 5,
-'multi_query_msg' : "<strong>Tip:</strong> Add items by URL for the perfect photo every time."
+'one_word_query_msg' : "<strong>Tip:</strong> Including brands and colors helps you find your item.",
+'min_entries_for_view_all' : 6
 }
