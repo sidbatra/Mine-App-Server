59db6160d2ed307996b58f7259ab725e2651546f
Deepak Rao <raodpk@gmail.com>
2012-03-16 00:09:37 +0000

    adding destroy observer to collections to delete og actions

--- app/observers/collection_observer.rb
+++ app/observers/collection_observer.rb
@@ -20,4 +20,13 @@ class CollectionObserver < ActiveRecord::Observer
       :update_collection,
       collection.id) if collection.reprocess
   end
+
+  # Delete facebook stories after a collection
+  # is deleted
+  #
+  def after_destroy(collection)
+    DistributionManager.delete_story(
+        collection.ticker_action,
+        collection.user.access_token) if collection.ticker_action
+  end
 end
