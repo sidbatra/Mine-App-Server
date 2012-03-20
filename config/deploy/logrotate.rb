f56f4610c875a6914a4456f75c23fe0a020b9574
Siddharth Batra <siddharthabatra@gmail.com>
2012-03-20 03:31:17 +0000

    basic deployment for search instances

--- config/deploy/logrotate.rb
+++ config/deploy/logrotate.rb
@@ -3,7 +3,7 @@ Capistrano::Configuration.instance(:must_exist).load do
   namespace :logrotate do
     
     desc "Install logrotate config"
-    task :install, :roles => [:web,:worker,:cron] do
+    task :install do
       run "sudo cp #{current_path}/config/logrotate/rails /etc/logrotate.d"
       run "sudo chown root:root /etc/logrotate.d/rails"
       run "sudo chmod 644 /etc/logrotate.d/rails"
