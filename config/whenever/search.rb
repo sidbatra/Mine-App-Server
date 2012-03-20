566d2ebdb72592c37c48895f199cd42d16550877
Siddharth Batra <siddharthabatra@gmail.com>
2012-03-20 04:21:21 +0000

    moving search cron job onto cron server

--- config/whenever/search.rb
+++
@@ -1,6 +0,0 @@
-set :output, "#{Dir.pwd}/log/cron.log"
-set :environment, ENV['RAILS_ENV']
-
-every 1.day, :at => '4:00pm' do
-  runner "ProcessingQueue.push(CronWorker,:maintain_search_index)"
-end
