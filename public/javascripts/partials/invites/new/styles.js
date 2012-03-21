c215bb2da18ddd89858413835a76c2b95b5b8f07
Deepak Rao <raodpk@gmail.com>
2012-03-21 02:16:34 +0000

    invite styles modified according to new ui

--- public/javascripts/partials/invites/new/styles.js
+++ public/javascripts/partials/invites/new/styles.js
@@ -5,11 +5,59 @@ Denwen.Partials.Invites.New.Styles = Backbone.View.extend({
   // Constructor logic
   //
   initialize: function() {
+    this.styles       = this.options.styles;
+    this.buttonEl     = '#styles_picked_button';
+
+    this.stylePickers = new Array();
+
+    this.setup();
   },
 
   // Fired just before the sub view comes into focus
   //
   display: function() {
+  },
+
+  // Setup pick/unpick functionality for all styles 
+  //
+  setup: function(){
+    var self = this;
+
+    this.styles.each(function(style){
+      var stylePicker = new Denwen.Partials.Styles.Picker({
+                                        model : style,
+                                        el    : $(self.stylesEl)
+                                        });
+
+      stylePicker.bind(
+        'stylePicked',
+        self.stylePicked,
+        self);
+
+      self.stylePickers.push(stylePicker);
+    });
+  },
+
+  // Fired when a style is picked
+  //
+  stylePicked: function(styleID) {
+    var self      = this;
+    this.styleID  = styleID;
+
+    $.each(this.stylePickers,function(i,stylePicker) {
+      if(self.styleID != stylePicker.model.get('id'))
+        stylePicker.disable();
+      else
+        stylePicker.enable();
+    });
+
+    $(this.buttonEl).attr('href','#styles-' + this.styleID + '/friends');
+    $(this.buttonEl).addClass('btn-primary');
+    $(this.buttonEl).removeAttr('disabled'); 
+    $(this.buttonEl).html(
+      "Next <i class='icon-chevron-right icon-white'></i>"); 
+
+    analytics.stylePicked();
   }
 
 });
