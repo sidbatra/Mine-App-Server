fe7396b6a1257fe815c376af6d0cd7ff6f332102
Siddharth Batra <siddharthabatra@gmail.com>
2012-03-21 04:40:52 +0000

    make infinite scroll more sensitive

--- public/javascripts/window_listener.js
+++ public/javascripts/window_listener.js
@@ -18,7 +18,7 @@ Denwen.WindowListener = Backbone.View.extend({
   //
   scroll: function() {
    if ($(window).scrollTop() >= $(document).height() - 
-                                  $(window).height() - 300)
+                                  $(window).height() - 500)
         this.trigger('documentScrolled');
   },
 
