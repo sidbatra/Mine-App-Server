2f41887b2b34f06bd3907afa5a71a9f7570fd0ff
Siddharth Batra <siddharthabatra@gmail.com>
2012-03-21 22:34:21 +0000

    removing cron task for users with no stores

--- app/controllers/admin/health_controller.rb
+++ app/controllers/admin/health_controller.rb
@@ -10,9 +10,9 @@ class Admin::HealthController < ApplicationController
       {
         :id => HealthReportService::FriendsDigest,
         :time => 1.day.ago},
-      {
-        :id => HealthReportService::TopShopperUpdate,
-        :time => 1.week.ago},
+      #{
+      #  :id => HealthReportService::TopShopperUpdate,
+      #  :time => 1.week.ago},
       {
         :id => HealthReportService::AnotherItemPrompt,
         :time => 1.week.ago},
@@ -22,9 +22,9 @@ class Admin::HealthController < ApplicationController
       {
         :id => HealthReportService::AddFriendsPrompt,
         :time => 3.days.ago},
-      {
-        :id => HealthReportService::AddStoresPrompt,
-        :time => 3.days.ago},
+      #{
+      #  :id => HealthReportService::AddStoresPrompt,
+      #  :time => 3.days.ago},
       {
         :id => HealthReportService::AddCollectionsPrompt,
         :time => 3.days.ago}]
