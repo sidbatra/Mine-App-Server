d893e278408da387163a7f49fbdc1d58a89b35d2
Deepak Rao <raodpk@gmail.com>
2012-03-21 22:06:51 +0000

    one word query msg changed in js config

--- public/javascripts/config.js
+++ public/javascripts/config.js
@@ -4,7 +4,7 @@
 // and rerun "rake js:dump:config"
 // 
 CONFIG = {
-'one_word_query_msg' : "<strong>Tip:</strong> Including brands and colors helps you find your item.",
+'one_word_query_msg' : "<strong>Tip:</strong> Try adding brands and colors, e.g. 'Vans Brown Tweed Slip ons'", 
 'multi_query_msg' : "<strong>Tip:</strong> Add items by URL for the perfect photo every time.",
 'multi_query_threshold' : 5,
 'minimum_products_in_collection' : 2,
