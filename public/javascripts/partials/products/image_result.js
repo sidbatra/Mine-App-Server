ed59047cf35f6666761ab72670a7d4b710256cd5
Siddharth Batra <siddharthabatra@gmail.com>
2012-03-21 04:39:19 +0000

    configuration for testing raw image sizes

--- public/javascripts/partials/products/image_result.js
+++ public/javascripts/partials/products/image_result.js
@@ -11,21 +11,22 @@ Denwen.Partials.Products.ImageResult = Backbone.View.extend({
   // Override render method for displaying view
   //
   render: function() {
-    var self      = this;
-    var thumbUrl  = this.model.get('medium_url');
-    this.divID    = Math.random().toString(36).substring(7);
+    var self        = this;
+    var thumbUrl    = this.model.get('medium_url');
+    this.imageTest  = this.options.imageTest;
+    this.divID      = Math.random().toString(36).substring(7);
 
     this.el.append(Denwen.JST['products/image_result']({
       thumbUrl:thumbUrl,
          divID:this.divID,
-          hide:false}));
+          hide:this.imageTest}));
 
     this.divEl = $('#' + this.divID);
 
     this.divEl.click(function(){self.clicked();});
 
-    //if(this.model.get('Filter'))
-    //  this.divEl.find("img").load(function(){self.imageLoaded(this)});
+    if(this.imageTest)
+      this.divEl.find("img").load(function(){self.imageLoaded(this)});
   },
 
   // Fired when the image is clicked
