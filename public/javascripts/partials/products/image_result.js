8ca2e7a62c33e3d86d28ed321e9bb46a47a9e56a
Siddharth Batra <siddharthabatra@gmail.com>
2012-03-21 04:22:25 +0000

    Hooking up new search service with products/new.
    Improvements to bing stabaility and edge case handling

--- public/javascripts/partials/products/image_result.js
+++ public/javascripts/partials/products/image_result.js
@@ -12,26 +12,26 @@ Denwen.Partials.Products.ImageResult = Backbone.View.extend({
   //
   render: function() {
     var self      = this;
-    var thumbUrl  = this.model.get('Thumbnail')['Url'];
+    var thumbUrl  = this.model.get('medium_url');
     this.divID    = Math.random().toString(36).substring(7);
 
     this.el.append(Denwen.JST['products/image_result']({
       thumbUrl:thumbUrl,
          divID:this.divID,
-          hide:this.model.get('Filter')}));
+          hide:false}));
 
     this.divEl = $('#' + this.divID);
 
     this.divEl.click(function(){self.clicked();});
 
-    if(this.model.get('Filter'))
-      this.divEl.find("img").load(function(){self.imageLoaded(this)});
+    //if(this.model.get('Filter'))
+    //  this.divEl.find("img").load(function(){self.imageLoaded(this)});
   },
 
   // Fired when the image is clicked
   //
   clicked: function() {
-    this.trigger('productImageClicked',this.model.infoHash());
+    this.trigger('productImageClicked',this.model);
   },
 
   // Fired when the image is loaded
