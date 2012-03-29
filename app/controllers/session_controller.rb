59aa742a40a8ea319ae0deceb792df93f07ab21e
Siddharth Batra <siddharthabatra@gmail.com>
2012-03-29 23:23:07 +0000

    session controller cleanup

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,29 +1,27 @@
-# Handle requests for the session lifecycle
-#
 class SessionController < ApplicationController
 
-  # Display the login page
-  #
-  def new
-  end
-
-  # Create a session by asking user to enter via facebook
+  # Redirect to a facebook url displaying the fb login dialog
+  # along with a callback (/facebook/reply) where the user is
+  # redirected after interacting with the fb dialog box. If the
+  # user has already authorized the app it goes directly to the
+  # callback url without any user interaction.
   #
   def create
-    fb_auth             = FbGraph::Auth.new(
-                            CONFIG[:fb_app_id],
-                            CONFIG[:fb_app_secret])
-    client              = fb_auth.client
+    fb_auth = FbGraph::Auth.new(
+                CONFIG[:fb_app_id],
+                CONFIG[:fb_app_secret])
+
+    client = fb_auth.client
     client.redirect_uri = fb_reply_url(
                             :src    => @source,
                             :target => params[:target])
 
-    target_url          = client.authorization_uri(
-                            :scope => [:email,
-                                       :user_likes,
-                                       :user_birthday,
-                                       :publish_actions,
-                                       :publish_stream])
+    target_url = client.authorization_uri(
+                  :scope => [:email,
+                             :user_likes,
+                             :user_birthday,
+                             :publish_actions,
+                             :publish_stream])
   rescue => ex
     handle_exception(ex)
     target_url = root_path(:src => HomeShowSource::LoginError)
@@ -32,13 +30,12 @@ class SessionController < ApplicationController
   end
 
   # Delete the current session and remove all existing cookie
-  # related data for the current user
+  # related data for the current user.
   #
   def destroy
     self.current_user.forget if logged_in?
     delete_cookie
     reset_session
-
   rescue => ex
     handle_exception(ex)
   ensure
