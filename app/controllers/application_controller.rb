aec4434f2aa0970f5c01ec4a953cd01df79c083e
Siddharth Batra <siddharthabatra@gmail.com>
2012-03-29 21:43:16 +0000

    cleaning up application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,31 +4,19 @@ class ApplicationController < ActionController::Base
   filter_parameter_logging :password, :secret 
   before_filter :track_source
 
-
   include ExceptionLoggable, DW::AuthenticationSystem, DW::RequestManagement
 
   protected
 
-  # Populate the source variable for analytics before
-  # every request
+  # Populate the @source variable before every request using the :src param.
+  # if :src isn't present set @source to 'direct'.
   #
   def track_source
     @source = params[:src] ? params[:src].to_s : 'direct'
   end
 
-  # Renew an active session after a certain time period to mark
-  # the user as an active user on the site
-  #
-  def renew_session
-    if logged_in? && (!session[:last_renewed_at] || 
-                    Time.now - session[:last_renewed_at] > 86400)
-      
-      self.current_user.touch
-      session[:last_renewed_at] = Time.now
-    end
-  end
-
-  # Prepare parameters for the image uploder
+  # Prepare uploader config for uploading directly to Amazon S3. Store
+  # the config as a hash into @upload_config.
   #
   def generate_uploader
     u = {}
