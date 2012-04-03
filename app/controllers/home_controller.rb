7100bf0a1864235db47ab4abaa8243cd078581ae
Siddharth Batra <siddharthabatra@gmail.com>
2012-04-03 00:28:39 +0000

    conditional root path display based on login state.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,18 @@
 class HomeController < ApplicationController
-  before_filter :detect_origin
-  layout        'home'
 
-  # Display the home page.
+  # Display the root path which is the feed is
+  # the user is logged in or the home page is
+  # the user isn't logged in.
   #
   def show
-    if params[:id]
-      redirect_to root_path
-    elsif logged_in? 
-      redirect_to user_path(
-                    self.current_user.handle,
-                    :src => UserShowSource::HomeRedirect)
+    if logged_in? 
+      render "feed/show", :layout => "application"
+    elsif 
+      detect_origin
+      redirect_to root_path if params[:id]
     end
   end
+
 
   private 
 
