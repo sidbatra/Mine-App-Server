3f1d72818927a2a425f712f567d672faaae7b6d9
Deepak Rao <raodpk@gmail.com>
2012-04-05 20:00:24 +0000

    comments and likes only shown if product has been shared on fb this starts the process of keeping facebook privacy settings inside our app as well

--- public/javascripts/views/products/show.js
+++ public/javascripts/views/products/show.js
@@ -10,14 +10,16 @@ Denwen.Views.Products.Show = Backbone.View.extend({
     this.product    = new Denwen.Models.Product(this.options.productJSON);
     this.source     = this.options.source;
 
-    this.likes = new Denwen.Partials.Likes.Likes();
-    this.likes.fetch(this.product.get('id'));
+    if(this.product.get('fb_object_id')) { 
+      this.likes = new Denwen.Partials.Likes.Likes();
+      this.likes.fetch(this.product.get('id'));
 
-    this.comments   = new Denwen.Partials.Comments.Comments();
-    this.comments.fetch(this.product.get('id'));
+      this.comments   = new Denwen.Partials.Comments.Comments();
+      this.comments.fetch(this.product.get('id'));
 
-    new Denwen.Partials.Likes.New({product_id:this.product.get('id')});
-    new Denwen.Partials.Comments.New({product_id:this.product.get('id')});
+      new Denwen.Partials.Likes.New({product_id:this.product.get('id')});
+      new Denwen.Partials.Comments.New({product_id:this.product.get('id')});
+    }
 
     new Denwen.Partials.Facebook.Base();
 
