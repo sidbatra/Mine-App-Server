01eb403eef2fdf63cf82b78852af47d19f1c6551
Siddharth Batra <siddharthabatra@gmail.com>
2012-04-07 01:07:45 +0000

    adding required fields into product select.

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -21,6 +21,7 @@ class FeedController < ApplicationController
 
     @products = Product.
                   select(:id,:created_at,:title,:handle,:endorsement,
+                          :orig_thumb_url,:orig_image_url,
                           :image_path,:is_processed,:user_id,:store_id,
                           :fb_action_id,:fb_photo_id).
                   with_user.
