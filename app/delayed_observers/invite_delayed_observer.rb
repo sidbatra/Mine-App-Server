0d1cff5be9540a29dc7936d6395d6ed7f34623d2
Deepak Rao <raodpk@gmail.com>
2012-04-10 18:01:03 +0000

    commenting out after create in invite delayed observer

--- app/delayed_observers/invite_delayed_observer.rb
+++ app/delayed_observers/invite_delayed_observer.rb
@@ -3,24 +3,24 @@ class InviteDelayedObserver < DelayedObserver
   # Delayed after_create.
   #
   def self.after_create(invite_id)
-    invite = Invite.find(invite_id)
+    #invite = Invite.find(invite_id)
 
     # Assuming name has atleast 2 parts
-    name_parts = invite.recipient_name.split(' ')
+    #name_parts = invite.recipient_name.split(' ')
 
-    user = User.add_from_invite({
-            :fb_user_id => invite.recipient_id,
-            :first_name => name_parts.first,
-            :last_name  => name_parts[1..-1].join(' ')})
+    #user = User.add_from_invite({
+    #        :fb_user_id => invite.recipient_id,
+    #        :first_name => name_parts.first,
+    #        :last_name  => name_parts[1..-1].join(' ')})
 
-    if user.valid?
-      Following.add(user.id,invite.user_id,FollowingSource::Invite,false)
-      Following.add(invite.user_id,user.id,FollowingSource::Invite,false)
+    #if user.valid?
+    #  Following.add(user.id,invite.user_id,FollowingSource::Invite,false)
+    #  Following.add(invite.user_id,user.id,FollowingSource::Invite,false)
 
       # Create invite
       #sender = User.find(invite.user_id)
       #DistributionManager.post_on_friends_wall(sender,user)
-    end
+    #end
   end
 
 end
