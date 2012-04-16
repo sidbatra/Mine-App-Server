2967b5e608eaa0b5321b3248b56d67aca1ef0aad
Pierre Legrain <pierrelegrain@gmail.com>
2012-04-16 01:52:53 +0000

    alert work - and feed display bug

--- public/javascripts/config.js
+++ public/javascripts/config.js
@@ -9,5 +9,5 @@ CONFIG = {
 'fb_publish_permissions' : "publish_actions,publish_stream",
 'multi_query_msg' : "<strong>Tip:</strong> Add items by URL for the perfect photo every time.",
 'multi_query_threshold' : 5,
-'one_word_query_msg' : "<strong>Tip:</strong> Try adding brands and colors, e.g. 'Vans Brown Tweed Slip ons'"
+'one_word_query_msg' : "<strong>Tip:</strong> Add brands and colors e.g. 'Nike mavrk grey sneaker'"
 }
