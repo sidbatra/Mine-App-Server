36986c3cc11d8a1bd402c0edf106c8e4f3112b1e
Siddharth Batra <siddharthabatra@gmail.com>
2012-04-19 01:08:21 +0000

    replacing old enum with constant string.

--- db/migrate/20120418010418_changing_default_values_for_fb_objects_in_products.rb
+++ db/migrate/20120418010418_changing_default_values_for_fb_objects_in_products.rb
@@ -1,10 +1,10 @@
 class ChangingDefaultValuesForFbObjectsInProducts < ActiveRecord::Migration
   def self.up
-    Product.update_all({:fb_action_id => FBSharing::Refuse}, :fb_action_id => nil)
-    Product.update_all({:fb_photo_id => FBSharing::Refuse}, :fb_photo_id => nil)
+    Product.update_all({:fb_action_id => '0'}, :fb_action_id => nil)
+    Product.update_all({:fb_photo_id => '0'}, :fb_photo_id => nil)
 
-    change_column :products, :fb_action_id, :string, :default => FBSharing::Refuse
-    change_column :products, :fb_photo_id, :string, :default => FBSharing::Refuse
+    change_column :products, :fb_action_id, :string, :default => '0'
+    change_column :products, :fb_photo_id, :string, :default => '0'
   end
 
   def self.down
