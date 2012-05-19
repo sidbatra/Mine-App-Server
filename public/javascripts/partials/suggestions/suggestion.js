f4aefae9767eccaedcb61281f2bc8e39671dea4d
Siddharth Batra <siddharthabatra@gmail.com>
2012-05-19 00:12:48 +0000

    interactions on suggestion jst.

--- public/javascripts/partials/suggestions/suggestion.js
+++ public/javascripts/partials/suggestions/suggestion.js
@@ -3,7 +3,16 @@ Denwen.Partials.Suggestions.Suggestion = Backbone.View.extend({
   // Constructor logic
   //
   initialize: function() {
+    var self = this;
+
+    this.suggestionEl = "#suggestion-" + this.model.get('id');
+    this.inputEl = "#suggestion-input-" + this.model.get('id');
+    this.submitEl = "#suggestion-button-" + this.model.get('id');
+
     this.render();
+
+    $(this.inputEl).keypress(function(e){self.inputKeypress(e)});
+    $(this.submitEl).click(function(){self.submitClicked()});
   },
 
   // Override render method for displaying view
@@ -12,10 +21,38 @@ Denwen.Partials.Suggestions.Suggestion = Backbone.View.extend({
     this.el.prepend(Denwen.JST['suggestions/suggestion']({
       suggestion:this.model}));
   },
+
+  // Fired when a key is pressed on the input.
+  //
+  inputKeypress: function(e) {
+    if(e.which == 13)
+      this.triggerSearchedCallback();
+  },
+
+  // Fired when the submit button is pressed.
+  //
+  submitClicked: function() {
+    this.triggerSearchedCallback();
+  },
+  
+  // Trigger a callback when the user initates
+  // a search via a suggestion.
+  //
+  triggerSearchedCallback: function() {
+
+    if(!$(this.inputEl).val().length)
+      return;
+
+    $(this.suggestionEl).fadeOut(300);
+
+    this.trigger(
+      Denwen.Partials.Suggestions.Suggestion.Callback.Searched,
+      this.model);
+  }
 });
 
 // Define callbacks.
 //
 Denwen.Partials.Suggestions.Suggestion.Callback = {
-  Clicked : 'clicked'
+  Searched : 'searched'
 }
