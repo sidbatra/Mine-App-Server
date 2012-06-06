66c0f7739508b722070e3021f9f962782e765279
Siddharth Batra <siddharthabatra@gmail.com>
2012-06-06 23:44:38 +0000

    user profiles no longer public.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :login_required, :only => :update
+  before_filter :login_required, :only => [:show,:update]
 
   # Create a new user.
   #
