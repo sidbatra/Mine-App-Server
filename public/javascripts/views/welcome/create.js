66a4bf193906eb9c0e7cb99f91eeb93755d75d86
Siddharth Batra <siddharthabatra@gmail.com>
2012-06-06 00:39:22 +0000

    purchase/input introduced in welcome create.

--- public/javascripts/views/welcome/create.js
+++ public/javascripts/views/welcome/create.js
@@ -10,6 +10,21 @@ Denwen.Views.Welcome.Create = Backbone.View.extend({
   // Constructor logic
   //
   initialize: function() {
+    this.input = new Denwen.Partials.Purchases.Input({
+                        el  : $('body'),
+                        mode: Denwen.PurchaseFormType.New});
+
+    this.input.bind(
+      Denwen.Partials.Purchases.Input.Callback.PurchaseCreated,
+      this.purchaseCreated,
+      this);
+
+    this.input.bind(
+      Denwen.Partials.Purchases.Input.Callback.PurchaseCreationFailed,
+      this.purchaseCreationFailed,
+      this);
+
+
     this.setAnalytics();
   },
 
@@ -17,6 +32,21 @@ Denwen.Views.Welcome.Create = Backbone.View.extend({
   //
   setAnalytics: function() {
     Denwen.Track.action("Welcome Create View");
+  },
+
+  // --
+  // Callbacks from Purchases.Input
+  // --
+
+  // Display the freshly created purchase in the feed.
+  //
+  purchaseCreated: function(purchase) {
+    Denwen.Track.action("Welcome Purchase Created");
+  },
+
+  // Display a creation error.
+  //
+  purchaseCreationFailed: function() {
   }
 
 });
