b966e502786554e218144046c28b3c2a2593d8e7
Siddharth Batra <siddharthabatra@gmail.com>
2012-06-29 01:21:20 +0000

    making static controller secure.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,7 +3,16 @@ class StaticController < ApplicationController
   # Display different static pages based on the aspect.
   #
   def show
-    render :partial => params[:aspect].to_s,
-           :layout  => "application"
+    
+    case params[:aspect].to_sym
+    when :copyright
+      @view = "copyright"
+    when :privacy
+      @view = "privacy"
+    when :terms
+      @view = "terms"
+    end
+
+    render @view
   end
 end
