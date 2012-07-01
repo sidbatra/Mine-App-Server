07d2a59e3e884f1ec48f729a25a84c9a854f644f
Siddharth Batra <siddharthabatra@gmail.com>
2012-07-01 22:50:34 +0000

    global queues for staging and production

--- config/initializers/z1_queue.rb
+++ config/initializers/z1_queue.rb
@@ -1,12 +1,12 @@
 
 FIFO::QueueManager.setup(CONFIG[:aws_access_id],CONFIG[:aws_secret_key])
 
-if Rails.env.start_with? 'p'
-  proc_queue_name = [Rails.env,Q[:proc]].join("_")
-  sec_proc_queue_name = [Rails.env,Q[:secondary_proc]].join("_")
-else
+if Rails.env.start_with? 'd'
   proc_queue_name = [Rails.env,CONFIG[:machine_id],Q[:proc]].join("_")
   sec_proc_queue_name = [Rails.env,CONFIG[:machine_id],Q[:secondary_proc]].join("_")
+else
+  proc_queue_name = [Rails.env,Q[:proc]].join("_")
+  sec_proc_queue_name = [Rails.env,Q[:secondary_proc]].join("_")
 end
 
 ProcessingQueue = FIFO::Queue.new proc_queue_name
