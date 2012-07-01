0ac61487d945b20ea6a8079d267036c311092bfb
Siddharth Batra <siddharthabatra@gmail.com>
2012-07-01 00:35:11 +0000

    separating out purchase show logged in and logged out views.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,6 +6,24 @@ module UsersHelper
     user.first_name + ' ' + user.last_name
   end
 
+  # Public. Geneder specific pronoun for the user.
+  #
+  # user - The User for which the pronoun is needed.
+  #
+  # Returns the String gender pronoun - his or her.
+  def user_pronoun(user)
+    user.is_male? ? 'his' : 'her'
+  end
+
+  # Public. Geneder specific singular pronoun for the user.
+  #
+  # user - The User for which the pronoun is needed.
+  #
+  # Returns the String gender singular pronoun - he or she.
+  def user_singular_pronoun(user)
+    user.is_male? ? 'he' : 'she'
+  end
+
   # Public: Gender of the user which defaults
   # to female incase the gender is unavailable.
   #
