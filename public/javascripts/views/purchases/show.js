06c0fcfb67183b2874fe4a13a696fc62883159a6
Siddharth Batra <siddharthabatra@gmail.com>
2012-07-01 00:52:03 +0000

    javascript for purchase show logged out and in.

--- public/javascripts/views/purchases/show.js
+++ public/javascripts/views/purchases/show.js
@@ -5,40 +5,36 @@ Denwen.Views.Purchases.Show = Backbone.View.extend({
   // Constructor logic
   //
   initialize: function() {
-    var self  = this;
+    var self = this;
 
-    this.purchase = new Denwen.Models.Purchase(this.options.purchaseJSON);
-    this.panel = this.options.panel;
-    this.source  = this.options.source;
-    this.friends = new Denwen.Collections.Users(this.options.friends);
-
-    this.owner   = this.purchase.get('user');
-    
-    this.interaction = this.friends.any(function(friend) {
-                        return friend.get('id') == self.owner.get('id');
-                       }) | Denwen.H.isCurrentUser(self.owner.get('id')); 
-
-    var purchaseDisplay = new Denwen.Partials.Purchases.Display({
-                              el : $('#feed'),
-                              interaction : this.interaction,
-                              model : this.purchase});
+    this.source = this.options.source;
 
     if(Denwen.H.isLoggedIn()) {
-      this.likes = new Denwen.Partials.Likes.Likes();
-      this.likes.fetch(this.purchase.get('id'));
+      this.purchase = new Denwen.Models.Purchase(this.options.purchaseJSON);
+      this.friends = new Denwen.Collections.Users(this.options.friends);
 
-      this.comments = new Denwen.Partials.Comments.Comments();
-      this.comments.fetch(this.purchase.get('id'));
+      this.owner = this.purchase.get('user');
+      
+      this.interaction = this.friends.any(function(friend) {
+                          return friend.get('id') == self.owner.get('id');
+                         }) | Denwen.H.isCurrentUser(self.owner.get('id')); 
+
+      var purchaseDisplay = new Denwen.Partials.Purchases.Display({
+                                el : $('#feed'),
+                                interaction : this.interaction,
+                                model : this.purchase});
+
+      if(this.interaction) {
+        this.likes = new Denwen.Partials.Likes.Likes();
+        this.likes.fetch(this.purchase.get('id'));
+
+        this.comments = new Denwen.Partials.Comments.Comments();
+        this.comments.fetch(this.purchase.get('id'));
+      }
 
       $('#purchase_thumb_' + this.purchase.get('id')).addClass('selected');
     }
-    else {
-      $('#purchase_panel_content_' + this.purchase.get('id')).html(this.panel);
-    }
     
-
-    new Denwen.Partials.Facebook.Base();
-
     this.loadFacebookPlugs();
 
     this.setAnalytics();
