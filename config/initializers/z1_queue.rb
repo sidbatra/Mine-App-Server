3cd9df7b3b5ad63e1c679e0c6fb71c87e9805f0f
Siddharth Batra <siddharthabatra@gmail.com>
2012-07-01 22:41:21 +0000

    switching to a fully abstracted queue.

--- config/initializers/z1_queue.rb
+++ config/initializers/z1_queue.rb
@@ -1,7 +1,5 @@
-include QueueManagement
 
-
-QueueManager.setup(CONFIG[:aws_access_id],CONFIG[:aws_secret_key])
+FIFO::QueueManager.setup(CONFIG[:aws_access_id],CONFIG[:aws_secret_key])
 
 if Rails.env.start_with? 'p'
   proc_queue_name = [Rails.env,Q[:proc]].join("_")
@@ -11,5 +9,5 @@ else
   sec_proc_queue_name = [Rails.env,CONFIG[:machine_id],Q[:secondary_proc]].join("_")
 end
 
-ProcessingQueue = QueueManagement::Queue.new proc_queue_name
-SecondaryProcessingQueue = QueueManagement::Queue.new sec_proc_queue_name
+ProcessingQueue = FIFO::Queue.new proc_queue_name
+SecondaryProcessingQueue = FIFO::Queue.new sec_proc_queue_name
