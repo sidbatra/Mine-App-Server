e1acf23cfdffcdcd8f6104042434be8e37d9e9d5
Siddharth Batra <siddharthabatra@gmail.com>
2012-07-04 15:12:11 +0000

    break feed cache after fb comment like update.

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -15,7 +15,7 @@ class FeedController < ApplicationController
     @before = params[:before] ? Time.at(params[:before].to_i) : nil
     @per_page = params[:per_page] ? params[:per_page].to_i : 10
 
-    @key = ["v1",self.current_user,
+    @key = ["v2",self.current_user,
             self.current_user.ifollowers.map(&:updated_at).max.to_i,
             "feed",@before ? @before.to_i : "",@per_page]
 
