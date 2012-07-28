1993398160a98b76641c100038b598b1a32068f0
Siddharth Batra <siddharthabatra@gmail.com>
2012-07-28 02:10:18 +0000

    removing index routes for comments & likes.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,33 +1,11 @@
 class CommentsController < ApplicationController
   before_filter :login_required
 
-  # List of native comments 
-  # 
-  # params[:purchase_ids] - Comma separated list for all purchase 
-  #                        ids whose comments need to be fetched
-  #
-  def index
-    purchase_ids  = params[:purchase_ids].split(",")
-    @comments     = Comment.with_user.for(purchase_ids)
-
-  rescue => ex
-    handle_exception(ex)
-  ensure
-    respond_to do |format|
-      format.json
-    end
-  end
-
   # Create a new comment
   #
   def create
-    comment   = Comment.add(params,self.current_user.id) 
+    @comment = Comment.add(params,self.current_user.id) 
 
-    @comment  = {:purchase_id  => comment.purchase_id,
-                 :id           => comment.id,
-                 :messsage     => comment.message,
-                 :from         => {:name => self.current_user.full_name,
-                                   :id   => self.current_user.fb_user_id}}
   rescue => ex
     handle_exception(ex)
   ensure
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,33 +1,11 @@
 class LikesController < ApplicationController
   before_filter :login_required, :except => :index
 
-  # List of native likes 
-  # 
-  # params[:purchase_ids] - Comma separated list for all purchase 
-  #                        ids whose likes need to be fetched
-  #
-  def index
-    purchase_ids  = params[:purchase_ids].split(",")
-    @likes        = Like.with_user.find_all_by_purchase_id(purchase_ids)
-
-  rescue => ex
-    handle_exception(ex)
-  ensure
-    respond_to do |format|
-      format.json
-    end
-  end
-
   # Create a new like 
   #
   def create
-    purchase_id = params[:purchase_id]
-    like        = Like.add(purchase_id,self.current_user.id)
+    @like = Like.add(params[:purchase_id],self.current_user.id)
 
-    @like = {:purchase_id => purchase_id,
-             :fb_user_id  => self.current_user.fb_user_id,
-             :name        => self.current_user.full_name,
-             :id          => like.id} 
   rescue => ex
     handle_exception(ex)
   ensure
