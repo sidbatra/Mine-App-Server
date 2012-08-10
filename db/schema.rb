42a9bcbdf6896d107aa208c8cdab127850d720d6
Deepak Rao <raodpk@gmail.com>
2012-08-10 23:29:37 +0000

    changing default value of share_to_facebook to false

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120810221442) do
+ActiveRecord::Schema.define(:version => 20120810232627) do
 
   create_table "comments", :force => true do |t|
     t.integer  "user_id"
@@ -196,7 +196,7 @@ ActiveRecord::Schema.define(:version => 20120810221442) do
 
   create_table "settings", :force => true do |t|
     t.integer  "user_id"
-    t.boolean  "share_to_facebook",  :default => true
+    t.boolean  "share_to_facebook",  :default => false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "email_influencer",   :default => true
