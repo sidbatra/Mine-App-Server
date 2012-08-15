103208ce48eb10beb2720d198590e0f4f7a1b2c8
Siddharth Batra <siddharthabatra@gmail.com>
2012-08-15 01:06:21 +0000

    user search partial.

---
+++ public/javascripts/partials/users/search.js
@@ -0,0 +1,98 @@
+Denwen.Partials.Users.Search = Backbone.View.extend({
+
+  //
+  //
+  initialize: function() {
+    var self = this;
+
+    this.query = "";
+    this.keyUpTimer = null;
+    this.users = new Denwen.Collections.Users();
+    this.usersHash = new Array();
+
+    this.el.keyup(function(e){self.keyup(e)});
+
+    this.el.typeahead({
+              items: 10,
+              item: '<li><a href="#"></a></li>',
+              updater: function(item) {
+                var user = self.usersHash[item];
+                window.location.href = "/" + user.get('handle') + "?src=search";
+                return item;
+              },
+              highlighter: function(item) {
+                var query = this.query.replace(/[\-\[\]{}()*+?.,\\\^$|#\s]/g, '\\$&');
+                var user = self.usersHash[item];
+                return "<img src='" + user.get('square_image_url') + "'/>" + 
+                        item.replace(new RegExp('(' + query + ')', 'ig'), function ($1, match) {
+                          return '<strong>' + match + '</strong>';
+                        });
+              },
+              sorter: function (items) {
+                return items;
+              },
+              matcher: function(item){
+                return ~item.toLowerCase().replace(/[^\w]/g,'').
+                          indexOf(this.query.toLowerCase().
+                                        replace(/[^\w]/g,''));}});
+  },
+
+  // Apply the given data source to the users collection
+  // and typeahead. Also initiates a new search.
+  //
+  applyDataSource: function(source) {
+    var self = this;
+
+    this.users = source;
+    this.usersHash = new Array();
+    
+    this.users.each(function(user){
+      self.usersHash[user.fullName()] = user; 
+    });
+
+    this.el.data('typeahead').source = this.users.map(function(user){return user.fullName()});
+    this.el.trigger('lookup');
+  },
+
+  // Handler for keystrokes on the input element.
+  //
+  keyup: function(e) {
+    
+    if(this.el.val() == this.query)
+      return;
+
+    this.query = this.el.val();
+
+    this.applyDataSource(new Denwen.Collections.Users());
+
+    if(this.keyUpTimer != null)
+      clearTimeout(this.keyUpTimer);
+  
+    var self = this;
+    this.keyUpTimer = setTimeout(
+                          function(){self.search(self.query);},
+                          300);
+    return true;
+  },
+
+  //
+  //
+  search: function(query) {
+    if(query.length < 2)
+      return;
+
+    var self = this;
+
+    this.users = new Denwen.Collections.Users();
+    this.users.fetch({
+          data      : {aspect: 'search',q: query},
+          success   : function(collection){self.searched();},
+          error     : function(collection,errors){}});
+  },
+
+  //
+  //
+  searched: function() {
+    this.applyDataSource(this.users);
+  }
+});
