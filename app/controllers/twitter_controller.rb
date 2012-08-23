015f921ece5a1ba64147ff41bf6670b3136917b6
Siddharth Batra <siddharthabatra@gmail.com>
2012-08-23 07:23:00 +0000

    abstract client creation.

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -1,13 +1,10 @@
 class TwitterController < ApplicationController
+  before_filter :create_client
 
   # Start the twitter authentication process.
   #
   def new
-    client = TwitterOAuth::Client.new(
-                :consumer_key     => CONFIG[:tw_consumer_key], 
-                :consumer_secret  => CONFIG[:tw_consumer_secret]) 
-
-    request_token = client.request_token(:oauth_callback => tw_reply_url)
+    request_token = @client.request_token(:oauth_callback => tw_reply_url)
     session[:tw_request_token] = request_token
     redirect_to request_token.authorize_url
 
@@ -18,14 +15,10 @@ class TwitterController < ApplicationController
   # Handle replies from twitter oauth.
   #
   def create
-    client = TwitterOAuth::Client.new(
-                :consumer_key     => CONFIG[:tw_consumer_key], 
-                :consumer_secret  => CONFIG[:tw_consumer_secret]) 
-
     oauth_verifier = params[:oauth_verifier]
 
     if oauth_verifier
-      access_token = client.authorize(
+      access_token = @client.authorize(
                               session[:tw_request_token].token,
                               session[:tw_request_token].secret,
                               :oauth_verifier => oauth_verifier)  
@@ -39,6 +32,17 @@ class TwitterController < ApplicationController
 
   rescue => ex
     handle_exception(ex)
+  end
+
+
+  private
+
+  # Create a twitter client variable.
+  #
+  def create_client
+    @client = TwitterOAuth::Client.new(
+                :consumer_key     => CONFIG[:tw_consumer_key], 
+                :consumer_secret  => CONFIG[:tw_consumer_secret]) 
   end
 
 end
