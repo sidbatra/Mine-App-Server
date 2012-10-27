f7ef574a0b50bbd11e18fb740e1a41d02d309c17
Siddharth Batra <siddharthabatra@gmail.com>
2012-10-27 20:36:07 +0000

    yahoo connect setup via omniauth.

--- app/controllers/yahoo_controller.rb
+++ app/controllers/yahoo_controller.rb
@@ -1,44 +1,24 @@
 class YahooController < ApplicationController
-  
-  def new
-    @target_url = yh_reply_url
-
-    @consumer = OAuth::Consumer.new(
-                                CONFIG[:yahoo_consumer_key],
-                                CONFIG[:yahoo_consumer_secret], 
-                                :site => 'https://api.login.yahoo.com',
-                                :access_token_path  => '/oauth/v2/get_token',
-                                :authorize_path     => '/oauth/v2/request_auth',
-                                :request_token_path => '/oauth/v2/get_request_token')
-
-    request_token = @consumer.get_request_token
-    session[:yahoo_request_token] = request_token
-
-    @target_url = request_token.authorize_url
-    redirect_to @target_url
-  end
 
   def create
-    raise params.to_yaml
-    #render :text => request.env['omniauth.auth'].to_yaml
-    #@usage = params[:usage] ? params[:usage].to_sym : :popup
-    #@error = params[:error]
+    @usage = params[:usage] ? params[:usage].to_sym : :popup
+    @error = params[:error]
 
-    #unless @error
-    #  case @usage
-    #  when :popup
-    #    credentials = request.env['omniauth.auth']['credentials']
-    #    info = request.env['omniauth.auth']['info']
+    unless @error
+      case @usage
+      when :popup
+        credentials = request.env['omniauth.auth']['credentials']
+        info = request.env['omniauth.auth']['info']
 
-    #    self.current_user.go_token = credentials['token']
-    #    self.current_user.go_secret = credentials['secret'] 
-    #    self.current_user.go_email = info['email']
-    #    self.current_user.save!
-    #  end
-    #end
+        self.current_user.yh_token = credentials['token']
+        self.current_user.yh_secret = credentials['secret'] 
+        self.current_user.yh_email = info['email']
+        self.current_user.save!
+      end
+    end
 
-  #rescue => ex
-  #  handle_exception(ex)
-  #ensure
+  rescue => ex
+    handle_exception(ex)
+  ensure
   end
 end
