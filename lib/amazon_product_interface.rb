c0dbc39cc4199150032a99867af31653b35c7183
Siddharth Batra <siddharthabatra@gmail.com>
2012-11-07 21:28:04 +0000

    spread aws product advertising api requests across multiple accounts.

--- lib/amazon_product_interface.rb
+++ lib/amazon_product_interface.rb
@@ -5,18 +5,21 @@ module DW
   #
   module AmazonProductInterface
     
-    # Setup options for accessing the api
-    #
-    Amazon::Ecs.configure do |options|
-      options[:associate_tag]     = CONFIG[:aws_associate_tag]
-      options[:AWS_access_key_id] = CONFIG[:aws_access_id]
-      options[:AWS_secret_key]    = CONFIG[:aws_secret_key]
-    end
 
     # Use the APA API to search products and get more
     # information about specific products
     #
     class AmazonProductSearch
+
+      def self.configure
+        index = rand CONFIG[:aws_associate_tags].length
+
+        Amazon::Ecs.configure do |options|
+          options[:associate_tag]     = CONFIG[:aws_associate_tags][index]
+          options[:AWS_access_key_id] = CONFIG[:aws_associate_access_ids][index]
+          options[:AWS_secret_key]    = CONFIG[:aws_associate_secret_keys][index]
+        end
+      end
 
       # Search amazon products matching a query with pagination.
       #
@@ -27,6 +30,8 @@ module DW
       # returns - Amazon::Ecs result object
       #
       def self.fetch_products(query,page=1,url_only=false)
+        configure
+
         result = Amazon::Ecs.item_search(
                   query, {
                     :response_group => 'Images,Small',
@@ -48,6 +53,8 @@ module DW
       # returns - Amazon::Ecs result object.
       #
       def self.lookup_products(ids,url_only=false)
+        configure
+
         ids = ids.join(",") if ids.is_a? Array
         result = Amazon::Ecs.item_lookup(ids,{
                     :response_group => 'Images,Small',
