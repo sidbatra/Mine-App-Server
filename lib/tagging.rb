771b941ff246b9fd2ea2f94ac02bed729663cb9c
Siddharth Batra <siddharthabatra@gmail.com>
2013-01-22 02:51:09 +0000

    populate tags for freshly added products.

--- lib/tagging.rb
+++ lib/tagging.rb
@@ -1,8 +1,7 @@
 module DW
 
   module Tagging
-
-
+    
     class ProductTagger
 
       def initialize(url,unique_id)
@@ -30,13 +29,19 @@ module DW
         @unique_id =~ /^AZ-/ || @url =~ /www\.amazon\./
       end
 
-      def amazon_tags
-        tags = nil
+      def amazon_id
         asn = nil
 
         asn = @unique_id[3..-1] if @unique_id.present?
         asn ||= @url.scan(/dp\/(\w+)/).flatten.first 
         asn ||= @url.scan(/gp\/product\/(\w+)/).flatten.first
+
+        asn
+      end
+
+      def amazon_tags
+        tags = nil
+        asn = amazon_id
 
         product = AmazonProductSearch.lookup_products(asn).first if asn
         tags = product.tags if product
